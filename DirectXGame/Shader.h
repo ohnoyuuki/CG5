3249c8153d4c45564c0e887f06073907e6ae041f
ohnoyuuki <k024g0074@g.neec.ac.jp>
2026-06-02 12:48:51 +0900

    t

---
+++ DirectXGame/Shader.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <d3d12.h>
+
+class Shader {
+
+public:
+	// シェーダーファイルを読み込み、コンパイル済みデータを生成する
+	void Load(const std::wstring& filePath, const std::string& shaderModel);
+
+	// 生成したコンパイル済みデータを取得する
+	ID3DBlob* GetBlob();
+
+	//コンストラクタ
+	Shader();
+	//デストラクタ
+	~Shader();
+};
