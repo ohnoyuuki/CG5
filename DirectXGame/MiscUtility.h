fa8dbaa9f2a02c29ee08a52be44cefeef8a2b63f
ohnoyuuki <k024g0074@g.neec.ac.jp>
2026-06-16 15:21:22 +0900

    t

--- DirectXGame/MiscUtility.h
+++ DirectXGame/MiscUtility.h
@@ -1,2 +1,13 @@
 #pragma once
-class MiscUtility {};
+#include<string>
+
+class MiscUtility {
+public:
+
+    // string =>wstring 文字列変換
+    std::wstring ConvertString(const std::string& str);
+
+    // wstring => string 文字列変換
+	std::string ConvertString(const std::wstring& str);
+
+};
