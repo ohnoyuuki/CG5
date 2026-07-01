8db91cf1bfe52a88819028504858e212eb6d29c4
ohnoyuuki <k024g0074@g.neec.ac.jp>
2026-07-01 12:24:04 +0900

    Kちょっと追加

---
+++ DirectXGame/IndexBuffer.cpp
@@ -0,0 +1,7 @@
+#include "IndexBuffer.h"
+
+void IndexBuffer::Create(const UINT size, const UINT stride) {}
+
+ID3D12Resource* IndexBuffer::Get() { return nullptr; }
+
+D3D12_INDEX_BUFFER_VIEW* IndexBuffer::GetView() { return nullptr; }
---
+++ DirectXGame/IndexBuffer.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<d3d12.h>
+
+class IndexBuffer {
+public:
+	//IndexBuffer生成
+	void Create(const UINT size, const UINT stride);
+
+	// ゲッター
+	ID3D12Resource* Get();//インデックスバッファー
+	D3D12_INDEX_BUFFER_VIEW* GetView(); // インデックスバッファービュー
+
+
+};
