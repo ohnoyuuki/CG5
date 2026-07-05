823934aecb2869c83d3769d04c532313ab31e486
ohnoyuuki <k024g0074@g.neec.ac.jp>
2026-07-05 23:18:51 +0900

    t

--- DirectXGame/TestVS.hlsl
+++ DirectXGame/TestVS.hlsl
@@ -1,3 +1,4 @@
+#include"TestVS.hlsl"
 
 struct VertexShaderOutput
 {
@@ -7,6 +8,7 @@ struct VertexShaderOutput
 struct VertexShaderInput
 {
     float32_t4 position : POSITION0;
+    float32_t2 texcoord : TEXCOORD0;
 };
 
 VertexShaderOutput main(VertexShaderInput input)
--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,9 +1,9 @@
+#include "IndexBuffer.h"
 #include "KamataEngine.h"
 #include "PipelineState.h"
 #include "Shader.h"
 #include "VertexBuffer.h"
 #include "rootSignature.h"
-#include "IndexBuffer.h"
 #include <Windows.h>
 
 // #include <d3dcompiler.h>
@@ -13,11 +13,16 @@ using namespace KamataEngine;
 // 関数プロトタイプ宣言
 void SetupPipelineState(PipelineState* pipelineState, RootSignature& rs, Shader& vs, Shader& ps) {
 	// InputLayout --------------------
-	D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[2] = {};
 	inputElementDescs[0].SemanticName = "POSITION";
 	inputElementDescs[0].SemanticIndex = 0;
 	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
 	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+
+	inputElementDescs[1].SemanticName = "TEXCOORD";
+	inputElementDescs[1].SemanticIndex = 0;
+	inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+	inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
 
 	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
 	inputLayoutDesc.pInputElementDescs = inputElementDescs;
@@ -95,16 +100,17 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	PipelineState pipelineState;
 	SetupPipelineState(&pipelineState, rs, vs, ps);
 
-		struct VertexData {
+	struct VertexData {
 		Vector4 position;
+		Vector2 texcoord;
 	};
 
 	// 頂点データの準備
 	VertexData vertices[] = {
-	    {{-1.0f, 1.0f, 0.0f, 1.0f}},  // v0 左上
-	    {{1.0f, 1.0f, 0.0f, 1.0f}},   // v1 右上
-	    {{-1.0f, -1.0f, 0.0f, 1.0f}}, // v2 左下
-	    {{1.0f, -1.0f, 0.0f, 1.0f}},  // v3 右下
+	    {{-1.0f, 1.0f, 0.0f, 1.0f},  {0.0f, 0.0f}}, // 左上
+	    {{1.0f, 1.0f, 0.0f, 1.0f},   {1.0f, 0.0f}}, // 右上
+	    {{-1.0f, -1.0f, 0.0f, 1.0f}, {0.0f, 1.0f}}, // 左下
+	    {{1.0f, -1.0f, 0.0f, 1.0f},  {1.0f, 1.0f}}, // 右下
 	};
 
 	// 頂点インデックスデータの準備
@@ -136,16 +142,14 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		pGpuIndices[i] = indices[i];
 	}
 
-
 	// 頂点リソースにデータを書き込む ------------------------------------------------------------------
-	//Vector4* vertexData = nullptr;
-	//vb.Get()->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
-	//vertexData[0] = {-0.5f, -0.5f, 0.0f, 1.0f}; // 左下
-	//vertexData[1] = {0.0f, 0.5f, 0.0f, 1.0f};   // 上
-	//vertexData[2] = {0.5f, -0.5f, 0.0f, 1.0f};  // 右下
+	// Vector4* vertexData = nullptr;
+	// vb.Get()->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+	// vertexData[0] = {-0.5f, -0.5f, 0.0f, 1.0f}; // 左下
+	// vertexData[1] = {0.0f, 0.5f, 0.0f, 1.0f};   // 上
+	// vertexData[2] = {0.5f, -0.5f, 0.0f, 1.0f};  // 右下
 	// 頂点リソースのマップを解除する
 	// vb.Get()->Unmap(0, nullptr);
-
 
 	// メインループ
 	while (true) {
