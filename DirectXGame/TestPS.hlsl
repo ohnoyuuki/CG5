f43bc007dbc5b156dd5ede2da006ea6e1d4ad465
ohnoyuuki <k024g0074@g.neec.ac.jp>
2026-07-22 10:40:37 +0900

    kansei

--- DirectXGame/TestPS.hlsl
+++ DirectXGame/TestPS.hlsl
@@ -15,9 +15,14 @@ PixelShaderOutput main(VertexShaderOutput input)
     float32_t2 uv = input.texcoord;
     float32_t4 textureColor = gTexture.Sample(gSampler, uv);
     
+    //grauscale
+    float32_t value = dot(textureColor.rgb, float32_t3(0.2125f, 0.7154f, 0.0721f));
+    output.color = float32_t4(value,value,value,textureColor.a);
+    
     ////位置セット(x y z w)かカラーセット(r g b a)でアクセスできる
-    output.color = textureColor;//non effect
+    //output.color = textureColor;//non effect
     //output.color.a = 1.0f; // Set alpha to 1.0 (fully opaque)
+    
     return output;
 }
 
--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -4,6 +4,7 @@
 #include "Shader.h"
 #include "VertexBuffer.h"
 #include "rootSignature.h"
+#include"WorldTransformEx.h"
 #include <Windows.h>
 
 // #include <d3dcompiler.h>
@@ -267,6 +268,22 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	PipelineState pipelineState;
 	SetupPipelineState(pipelineState, rs, vs, ps);
 
+	// アプリで利用する3Dモデル ----------------------------------
+	//被写体の準備
+	Model* model = Model::CreateFromOBJ("terrain");
+
+	
+	WorldTransformEx worldTransform;
+	worldTransform.Initialize();
+	worldTransform.scale_ = Vector3(1.0f, 1.0f, 1.0f);
+
+	// カメラの準備
+	Camera camera;
+	camera.Initialize();
+	camera.translation_ = Vector3(0.0f, 1.0f, 0.0f);
+
+
+
 	struct VertexData {
 		Vector4 position;
 		Vector2 texcoord;
@@ -318,12 +335,23 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	// 頂点リソースのマップを解除する
 	// vb.Get()->Unmap(0, nullptr);
 
+
+
+
 	// メインループ
 	while (true) {
 		// エンジンの更新
 		if (KamataEngine::Update()) {
 			break; // Updateがtrueを返したらループを抜ける
 		}
+
+		//world変換行列の定数バッファへの転送
+		worldTransform.rotation_.y += 0.005f;//適当な回転角度(ラジアン)
+		worldTransform.UpdateMatrix();
+
+		//cameraの更新と定数バッファへの転送
+		camera.UpdateMatrix();
+
 
 		// TransitionBarrierを SRV → RTV に設定する
 		D3D12_RESOURCE_BARRIER barrier{};
@@ -363,7 +391,13 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		// 指定した深度で画面全体をクリアする
 		commandList->ClearDepthStencilView(dsvHandleCPU, D3D12_CLEAR_FLAG_DEPTH, 1.0f, 0, 0, nullptr);
 
-			// TransitionBarrierを元に戻し、PixelShaderが扱えるようにする
+		//描画
+		Model::PreDraw();
+		model->Draw(worldTransform, camera);
+		Model::PostDraw();
+
+
+		// TransitionBarrierを元に戻し、PixelShaderが扱えるようにする
 		barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;                      // TranslationBarrierの設定
 		barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;                           // フラグは None にしておく
 		barrier.Transition.pResource = renderTextureResource;                       // バリアを張る対象のリソース
@@ -371,7 +405,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE; // 遷移後
 		commandList->ResourceBarrier(1, &barrier); 
 
-		// 描画前処理
+		// 描画前処理（開始）
 		dxCommon->PreDraw();
 		// ここに描画処理を記述する
 
@@ -397,12 +431,16 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	}
 
 	// 解放
+	delete model;
+
 	renderTextureResource->Release();
 	srvDescriptorHeap->Release();
 	rtvDescriptorHeap->Release();
 
 	depthStencilResource->Release();
 	dsvDescriptorHeap->Release();
+
+
 
 	// エンジンの終了処理
 	KamataEngine::Finalize();
